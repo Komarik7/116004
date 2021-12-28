a2b1be7e2a218ea9e7b2445f488efa6cf2a56523
Komarik7 <91026534+Komarik7@users.noreply.github.com>
2021-12-28 19:51:01 +0300

    Add files via upload

---
+++ HW1_Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Стих
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Сергей Есенин Мои мечты");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Мои мечты стремятся вдаль,");
+            Console.WriteLine ("Чужую разделить печаль");
+            Console.WriteLine("Где слышны вопли и рыданья");
+            Console.WriteLine( "И муки тяжкого страданья.");
+            Console.WriteLine( "Я там могу найти себе");
+            Console.WriteLine("Отраду в жизни, упоенье,");
+            Console.WriteLine ("И там, наперекор судьбе,");
+            Console.WriteLine("Искать я буду вдохновенья") ;
+            
+            
+        }
+
+    }
+}
