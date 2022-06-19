0651ec927a1778dd65850be7a7bb7d3af08dc008
Komarik7 <artkomarov@mail.ru>
2022-06-20 03:12:09 +0500

    cs v8

---
+++ Program1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Зачёт_В8
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
